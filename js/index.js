bc3966b61bafb3daf459c03b6677d0d4a9bba2fb
EsraaAtef3 <esraaatef375@gmail.com>
2023-03-18 00:26:10 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,487 @@
+let newData = document.getElementById("newData");
+let searchPage = document.getElementById("searchPage");
+let submitBtn;
+
+$(document).ready(() => {
+    searchByName("").then(() => {
+        $(".loadingScreen").fadeOut(500)
+        $("body").css("overflow", "visible")
+
+    })
+})
+
+function openSideNav() {
+    $(".sideNavMenu").animate({
+        left: 0
+    }, 500)
+
+
+    $(".open-close-icon").removeClass("fa-align-justify");
+    $(".open-close-icon").addClass("fa-x");
+
+
+    for (let i = 0; i < 5; i++) {
+        $(".links li").eq(i).animate({
+            top: 0
+        }, (i + 5) * 100)
+    }
+}
+
+function closeSideNav() {
+    let boxWidth = $(".sideNavMenu .navTab").outerWidth()
+    $(".sideNavMenu").animate({
+        left: -boxWidth
+    }, 500)
+
+    $(".open-close-icon").addClass("fa-align-justify");
+    $(".open-close-icon").removeClass("fa-x");
+
+
+    $(".links li").animate({
+        top: 300
+    }, 500)
+}
+
+closeSideNav()
+$(".sideNavMenu i.open-close-icon").click(() => {
+    if ($(".sideNavMenu").css("left") == "0px") {
+        closeSideNav()
+    } else {
+        openSideNav()
+    }
+})
+
+
+function getMeals(arr) {
+    let cartoona = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        cartoona += `
+        <div class="col-md-3">
+                <div onclick="getDetails('${arr[i].idMeal}')" class="meal position-relative overflow-hidden rounded-2 cursor-pointer">
+                    <img class="w-100" src="${arr[i].strMealThumb}" alt="" srcset="">
+                    <div class="meal-layer position-absolute d-flex align-items-center text-black p-2">
+                        <h3>${arr[i].strMeal}</h3>
+                    </div>
+                </div>
+        </div>
+        `
+    }
+
+    newData.innerHTML = cartoona
+}
+
+async function getDetails(mealID) {
+    closeSideNav()
+    newData.innerHTML = ""
+    $(".inner-loadingScreen").fadeIn(300)
+
+    searchPage.innerHTML = "";
+    let respone = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`);
+    respone = await respone.json();
+
+    displayDetails(respone.meals[0])
+    $(".inner-loadingScreen").fadeOut(300)
+
+}
+
+function displayDetails(meal) {
+    
+    searchPage.innerHTML = "";
+
+
+    let ingredients = ``
+
+    for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients += `<li class="alert alert-info m-2 p-1">${meal[`strMeasure${i}`]} ${meal[`strIngredient${i}`]}</li>`
+        }
+    }
+
+    let tags = meal.strTags?.split(",")
+    // let tags = meal.strTags.split(",")
+    if (!tags) tags = []
+
+    let tagsStr = ''
+    for (let i = 0; i < tags.length; i++) {
+        tagsStr += `
+        <li class="alert alert-danger m-2 p-1">${tags[i]}</li>`
+    }
+
+    let cartoona = `
+    <div class="col-md-4">
+                <img class="w-100 rounded-3" src="${meal.strMealThumb}"
+                    alt="">
+                    <h2>${meal.strMeal}</h2>
+            </div>
+            <div class="col-md-8">
+                <h2>Instructions</h2>
+                <p>${meal.strInstructions}</p>
+                <h3><span class="fw-bolder">Area : </span>${meal.strArea}</h3>
+                <h3><span class="fw-bolder">Category : </span>${meal.strCategory}</h3>
+                <h3>Recipes :</h3>
+                <ul class="list-unstyled d-flex g-3 flex-wrap">
+                    ${ingredients}
+                </ul>
+
+                <h3>Tags :</h3>
+                <ul class="list-unstyled d-flex g-3 flex-wrap">
+                    ${tagsStr}
+                </ul>
+
+                <a target="_blank" href="${meal.strSource}" class="btn btn-success">Source</a>
+                <a target="_blank" href="${meal.strYoutube}" class="btn btn-danger">Youtube</a>
+            </div>`
+
+    newData.innerHTML = cartoona
+}
+
+
+function showSearchInputs() {
+    searchPage.innerHTML = `
+    <div class="row py-4 ">
+        <div class="col-md-6 ">
+            <input onkeyup="searchByName(this.value)" class="form-control bg-transparent text-white" type="text" placeholder="Search By Name">
+        </div>
+        <div class="col-md-6">
+            <input onkeyup="searchByFirstLetter(this.value)" maxlength="1" class="form-control bg-transparent text-white" type="text" placeholder="Search By First Letter">
+        </div>
+    </div>`
+
+    newData.innerHTML = ""
+}
+
+
+async function searchByFirstLetter(term) {
+    closeSideNav()
+    newData.innerHTML = ""
+    $(".inner-loadingScreen").fadeIn(300)
+
+    term == "" ? term = "a" : "";
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${term}`)
+    response = await response.json()
+
+    response.meals ? getMeals(response.meals) : getMeals([])
+    $(".inner-loadingScreen").fadeOut(300)
+
+}
+
+async function searchByName(term) {
+    closeSideNav()
+    newData.innerHTML = ""
+    $(".inner-loadingScreen").fadeIn(300)
+
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+    response = await response.json()
+
+    response.meals ? getMeals(response.meals) : getMeals([])
+    $(".inner-loadingScreen").fadeOut(300)
+
+}
+
+async function getCategories() {
+    newData.innerHTML = ""
+    $(".inner-loadingScreen").fadeIn(300)
+    searchPage.innerHTML = "";
+
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
+    response = await response.json()
+
+    displayCategories(response.categories)
+    $(".inner-loadingScreen").fadeOut(300)
+
+}
+
+function displayCategories(arr) {
+    let cartoona = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        cartoona += `
+        <div class="col-md-3">
+                <div onclick="getCategoryMeals('${arr[i].strCategory}')" class="meal position-relative overflow-hidden rounded-2 cursor-pointer">
+                    <img class="w-100" src="${arr[i].strCategoryThumb}" alt="" srcset="">
+                    <div class="meal-layer position-absolute text-center text-black p-2">
+                        <h3>${arr[i].strCategory}</h3>
+                        <p>${arr[i].strCategoryDescription.split(" ").slice(0,20).join(" ")}</p>
+                    </div>
+                </div>
+        </div>
+        `
+    }
+
+    newData.innerHTML = cartoona
+}
+
+async function getArea() {
+    newData.innerHTML = ""
+    $(".inner-loadingScreen").fadeIn(300)
+
+    searchPage.innerHTML = "";
+
+    let respone = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`)
+    respone = await respone.json()
+    console.log(respone.meals);
+
+    displayArea(respone.meals)
+    $(".inner-loadingScreen").fadeOut(300)
+
+}
+
+function displayArea(arr) {
+    let cartoona = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        cartoona += `
+        <div class="col-md-3">
+                <div onclick="getAreaMeals('${arr[i].strArea}')" class="rounded-2 text-center cursor-pointer">
+                        <i class="fa-solid fa-house-laptop fa-4x"></i>
+                        <h3>${arr[i].strArea}</h3>
+                </div>
+        </div>
+        `
+    }
+
+    newData.innerHTML = cartoona
+}
+
+async function getAreaMeals(area) {
+    newData.innerHTML = ""
+    $(".inner-loadingScreen").fadeIn(300)
+
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`)
+    response = await response.json()
+
+
+    getMeals(response.meals.slice(0, 20))
+    $(".inner-loadingScreen").fadeOut(300)
+
+}
+
+
+async function getIngredients() {
+    newData.innerHTML = ""
+    $(".inner-loadingScreen").fadeIn(300)
+
+    searchPage.innerHTML = "";
+
+    let respone = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?i=list`)
+    respone = await respone.json()
+    console.log(respone.meals);
+
+    displayIngredients(respone.meals.slice(0, 20))
+    $(".inner-loadingScreen").fadeOut(300)
+
+}
+
+function displayIngredients(arr) {
+    let cartoona = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        cartoona += `
+        <div class="col-md-3">
+                <div onclick="getIngredientsMeals('${arr[i].strIngredient}')" class="rounded-2 text-center cursor-pointer">
+                        <i class="fa-solid fa-drumstick-bite fa-4x"></i>
+                        <h3>${arr[i].strIngredient}</h3>
+                        <p>${arr[i].strDescription.split(" ").slice(0,20).join(" ")}</p>
+                </div>
+        </div>
+        `
+    }
+
+    newData.innerHTML = cartoona
+}
+
+async function getCategoryMeals(category) {
+    newData.innerHTML = ""
+    $(".inner-loadingScreen").fadeIn(300)
+
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
+    response = await response.json()
+
+
+    getMeals(response.meals.slice(0, 20))
+    $(".inner-loadingScreen").fadeOut(300)
+
+}
+
+async function getIngredientsMeals(ingredients) {
+    newData.innerHTML = ""
+    $(".inner-loadingScreen").fadeIn(300)
+
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredients}`)
+    response = await response.json()
+
+
+    getMeals(response.meals.slice(0, 20))
+    $(".inner-loadingScreen").fadeOut(300)
+
+}
+function showContacts() {
+    newData.innerHTML = `<div class="contact min-vh-100 d-flex justify-content-center align-items-center">
+    <div class="container w-75 text-center">
+        <div class="row g-4">
+            <div class="col-md-6">
+                <input id="getName" onkeyup="inputsValidation()" type="text" class="form-control" placeholder="Enter Your Name">
+                <div id="nameAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Special characters and numbers not allowed
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input id="getEmail" onkeyup="inputsValidation()" type="email" class="form-control " placeholder="Enter Your Email">
+                <div id="emailAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Email not valid *exemple@yyy.zzz
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input id="getPhone" onkeyup="inputsValidation()" type="text" class="form-control " placeholder="Enter Your Phone">
+                <div id="phoneAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid Phone Number
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input id="ageInput" onkeyup="inputsValidation()" type="number" class="form-control " placeholder="Enter Your Age">
+                <div id="ageAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid age
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input  id="getPassword" onkeyup="inputsValidation()" type="password" class="form-control " placeholder="Enter Your Password">
+                <div id="passwordAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid password *Minimum eight characters, at least one letter and one number:*
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input  id="getRepassword" onkeyup="inputsValidation()" type="password" class="form-control " placeholder="Repassword">
+                <div id="repasswordAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid repassword 
+                </div>
+            </div>
+        </div>
+        <button id="submitBtn" disabled class="btn btn-outline-danger px-2 mt-3">Submit</button>
+    </div>
+</div> `
+    submitBtn = document.getElementById("submitBtn")
+
+
+    document.getElementById("getName").addEventListener("focus", () => {
+        getNameMatched = true
+    })
+
+    document.getElementById("getEmail").addEventListener("focus", () => {
+        getEmailMatched = true
+    })
+
+    document.getElementById("getPhone").addEventListener("focus", () => {
+        getPhoneMatched = true
+    })
+
+    document.getElementById("ageInput").addEventListener("focus", () => {
+        getAgeMatched = true
+    })
+
+    document.getElementById("getPassword").addEventListener("focus", () => {
+        getPasswordMatched = true
+    })
+
+    document.getElementById("getRepassword").addEventListener("focus", () => {
+        getRepasswordMatched = true
+    })
+}
+
+let getNameMatched = false;
+let getEmailMatched = false;
+let getPhoneMatched = false;
+let getAgeMatched = false;
+let getPasswordMatched = false;
+let getRepasswordMatched = false;
+
+function inputsValidation() {
+    if (getNameMatched) {
+        if (nameValidation()) {
+            document.getElementById("nameAlert").classList.replace("d-block", "d-none")
+
+        } else {
+            document.getElementById("nameAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+    if (getEmailMatched) {
+
+        if (emailValidation()) {
+            document.getElementById("emailAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("emailAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (getPhoneMatched) {
+        if (phoneValidation()) {
+            document.getElementById("phoneAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("phoneAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (getAgeMatched) {
+        if (ageValidation()) {
+            document.getElementById("ageAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("ageAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (getPasswordMatched) {
+        if (passwordValidation()) {
+            document.getElementById("passwordAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("passwordAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+    if (getRepasswordMatched) {
+        if (repasswordValidation()) {
+            document.getElementById("repasswordAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("repasswordAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (nameValidation() &&
+        emailValidation() &&
+        phoneValidation() &&
+        ageValidation() &&
+        passwordValidation() &&
+        repasswordValidation()) {
+        submitBtn.removeAttribute("disabled")
+    } else {
+        submitBtn.setAttribute("disabled", true)
+    }
+}
+
+function nameValidation() {
+    return (/^[a-zA-Z ]+$/.test(document.getElementById("getName").value))
+}
+
+function emailValidation() {
+    return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById("getEmail").value))
+}
+
+function phoneValidation() {
+    return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(document.getElementById("getPhone").value))
+}
+
+function ageValidation() {
+    return (/^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/.test(document.getElementById("getAge").value))
+}
+
+function passwordValidation() {
+    return (/^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/.test(document.getElementById("getPassword").value))
+}
+
+function repasswordValidation() {
+    return document.getElementById("getRepassword").value == document.getElementById("getPassword").value
+}
